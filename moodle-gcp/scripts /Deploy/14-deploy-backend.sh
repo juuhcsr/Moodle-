b513b7be5c5783dbd6098971009eda9bea1a7df0
Juuhcsr <juuhcsr@hotmail.com>
2023-02-23 10:22:28 -0300

    teste_1_f

--- moodle-gcp/scripts /Deploy/14-deploy-backend.sh
+++ moodle-gcp/scripts /Deploy/14-deploy-backend.sh
@@ -8,7 +8,7 @@ gcloud container clusters get-credentials $GKE_NAME \
     --region $REGION \
     --project $PROJECT_ID
     
-kubectl apply -f /deploy/moodle-on-gcp/6-backendconfig/ingress-backendconfig.yaml    
+kubectl apply -f ./deploy/moodle-on-gcp/6-backendconfig/ingress-backendconfig.yaml    
 
 echo "Press CTRL+C to exit..."
 echo "<a href='https://github.com/google/moodle-on-gcp/blob/main/docs/deploying-backend-config.md'>Documentation</a>"
